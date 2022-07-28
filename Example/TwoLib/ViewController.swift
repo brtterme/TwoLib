5300be65a3d9b46bc714af2177e849f61cde6857
better me <lili.zhao@firstai.ai>
2022-07-28 16:15:07 +0800

    Initial commit

---
+++ Example/TwoLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TwoLib
+//
+//  Created by 2950024989@qq.com on 07/28/2022.
+//  Copyright (c) 2022 2950024989@qq.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
